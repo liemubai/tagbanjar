c25dffc60a6549549257d2735b68f8dc0f71ceda
liemubai <liemubai@gmail.com>
2023-06-12 21:16:42 +0800

    first commit

---
+++ tagger/errors.py
@@ -0,0 +1,12 @@
+__author__ = 'liemubai'
+class Error(Exception):
+   """Base class for other exceptions"""
+   pass
+
+class StopWordMustList(Error):
+   """Raised when the input stopword value is not a list"""
+   pass
+
+class NGramErr(Error):
+   """Raised For Error in N-Grams Modeling"""
+   pass
